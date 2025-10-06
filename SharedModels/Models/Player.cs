4dff3743979288062a8757e3b9722f992115b3ed
VeKTyS <gtastyle.731@gmail.com>
2025-10-06 09:58:49 +0200

    Création front (sans logique)

--- SharedModels/Models/Player.cs
+++ SharedModels/Models/Player.cs
@@ -5,5 +5,7 @@ public class Player
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Username { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
     public int TotalScore { get; set; }
 }
